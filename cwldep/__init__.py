91d23f8d4f03d44d398978cbce3bf6929417a9e3
John Bradley <johnbradley2008@gmail.com>
2018-06-29 13:21:39 -0700

    move urls to the top
    reduces redundancy of urls in __init__.py

--- cwldep/__init__.py
+++ cwldep/__init__.py
@@ -29,6 +29,10 @@ from schema_salad.sourceline import cmap
 
 logging.basicConfig(level=logging.INFO)
 
+CWLDEP_URL = "http://commonwl.org/cwldep#"
+CWLDEP_DEPENDENCIES_URL = "{}Dependencies".format(CWLDEP_URL)
+
+
 def download(tgt, url, version, locks, verified, check_only):
     dltgt = tgt + "_download_"
 
@@ -153,7 +157,7 @@ def cwl_deps(basedir, dependencies, locks, verified, operation):
                 def do_deps(req):
                     cwl_deps(installTo, req, locks, verified, operation)
 
-                visit_class(document, ("http://commonwl.org/cwldep#Dependencies",), do_deps)
+                visit_class(document, (DEP_NAMESPACE_URL,), do_deps)
 
             elif spup.path.endswith(".tar.gz") or spup.path.endswith(".tar.bz2") or spup.path.endswith(".zip"):
                 download(tgt, upstream, "", locks, verified, operation=="check")
@@ -224,7 +228,7 @@ def add_dep(fn, upstream, set_version, install_to):
             obj["installTo"] = install_to
         if isinstance(hints, list):
             for h in hints:
-                if expand_ns(namespaces, h["class"]) == "http://commonwl.org/cwldep#Dependencies":
+                if expand_ns(namespaces, h["class"]) == DEP_NAMESPACE_URL:
                     for u in h["dependencies"]:
                         if u["upstream"] == upstream:
                             u.update(obj)
@@ -235,7 +239,7 @@ def add_dep(fn, upstream, set_version, install_to):
                                "dependencies": [obj]}))
         elif isinstance(hints, dict):
             for h in hints:
-                if expand_ns(namespaces, h) == "http://commonwl.org/cwldep#Dependencies":
+                if expand_ns(namespaces, h) == DEP_NAMESPACE_URL:
                     for u in hints[h]["dependencies"]:
                         if u["upstream"] == upstream:
                             u.update(obj)
@@ -246,7 +250,7 @@ def add_dep(fn, upstream, set_version, install_to):
 
     visit_class(workflowobj, ("Workflow",), _add)
 
-    namespaces["dep"] = "http://commonwl.org/cwldep#"
+    namespaces["dep"] = CWLDEP_URL
     workflowobj["$namespaces"] = namespaces
 
     del workflowobj["id"]
@@ -288,7 +292,7 @@ def main():
     def do_deps(req):
         cwl_deps(os.getcwd(), req, locks, verified, args.operation)
 
-    visit_class(document, ("http://commonwl.org/cwldep#Dependencies",), do_deps)
+    visit_class(document, (CWLDEP_DEPENDENCIES_URL,), do_deps)
 
     unref = False
     for l in locks:
